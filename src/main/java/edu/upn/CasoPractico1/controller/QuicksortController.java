bc56ccea9f84b42d770c555b346c585d426886cd
chiszo <gigibethandrea@gmail.com>
2025-10-25 09:42:41 -0500

    pagina de quicksort

--- src/main/java/edu/upn/CasoPractico1/controller/QuicksortController.java
+++ src/main/java/edu/upn/CasoPractico1/controller/QuicksortController.java
@@ -1,14 +1,13 @@
 package edu.upn.CasoPractico1.controller;
 
-
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
-public class Home {
-    @GetMapping("/inicio")
+public class QuicksortController {
+    @GetMapping("/quicksort")
     public String Inicio(){
-        return "inicio";
+        return "quicksort";
     }
 }
