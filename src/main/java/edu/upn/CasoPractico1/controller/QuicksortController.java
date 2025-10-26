25636d19c2bfc6429f5a328f3d5a690ca7def36d
chiszo <gigibethandrea@gmail.com>
2025-10-26 01:21:28 -0500

    ordenacion quicksort

--- src/main/java/edu/upn/CasoPractico1/controller/QuicksortController.java
+++ src/main/java/edu/upn/CasoPractico1/controller/QuicksortController.java
@@ -2,14 +2,44 @@ package edu.upn.CasoPractico1.controller;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class QuicksortController {
 
+    private List<Integer> a = new ArrayList<>();
 
     @GetMapping("/quicksort")
     public String Inicio(){
+        return "quicksort";
+    }
+
+    @PostMapping("/agregar")
+    public String MetodoQuicksort(@RequestParam("numeros") int numero,Model model){
+        a.add(numero);
+        model.addAttribute("numeros", a);
+        return "quicksort";
+    }
+
+    @PostMapping("/quicksort")
+    public String ordenacion(Model model){
+        if (!a.isEmpty()) {
+            int[] arreglo = a.stream().mapToInt(Integer::intValue).toArray();
+            quicksort(arreglo, 0, arreglo.length - 1);
+
+            // actualizamos la lista 'a' con los valores ordenados
+            a.clear();
+            for (int n : arreglo) {
+                a.add(n);
+            }
+        }
+
+        model.addAttribute("numeros", a);
         return "quicksort";
     }
 
